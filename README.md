22f53119dbaf48580959d687cd694c231078101e
babasahebpinjar <babasahebpinjar@gmail.com>
2024-08-29 09:56:41 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,102 @@
+
+# Chatbot Conversational Form
+Chatbot Conversational Form is a demo project that simplifies creating interactive forms using a chatbot-style interface for tasks like surveys, questionnaires, and feedback forms.
+
+## Features
+- Collect User Information: The chatbot gathers user information in a natural and non-intrusive manner. It explains why it needs specific details from a predefined list.
+
+- Empathetic Explanation: The chatbot kindly explains why it requests information, creating a user-friendly experience.
+
+- Rapport Building: The chatbot engages in small talk to build rapport with users, maintaining a friendly atmosphere.
+
+- Handling User Hesitation: The chatbot provides reassurance and alternatives if users hesitate, making corrections easy.
+
+- Thankful and Supportive: When the task is complete, the chatbot expresses gratitude and offers further assistance.
+
+- Conversation Workflow: The chatbot follows a clear and engaging conversation structure, avoiding list questions.
+
+- Privacy and User-Centric: The chatbot prioritizes user comfort and data privacy.
+
+
+
+## Tech Functionality
+Used langchain's ConversationChain for chatbot and create_tagging_chain_pydantic for extract and validation of user input.
+
+
+## Installation
+
+To get started with Chatbot Conversational Form, you'll need Python 3.7+ installed. You can install the required dependencies using pip:
+
+```bash
+pip install -r requirements.txt
+```
+
+For Supporting memorization of user data, you need to use mongodb:
+## Set up MongoDB to connect with Langchain
+First you will configure a MongoDB database either locally or on the cloud. For this tutorial, we will use MongoDB Cloud, but if you prefer to set it up locally, you can follow the MongoDB installation instructions for your operating system.
+
+Here’s a detailed description of setting up MongoDB Cloud:
+
+1. Visit the MongoDB website and sign in to your account or create a new account if you don’t have one already.
+2. Once you are logged in, navigate to the MongoDB Cloud or Atlas dashboard.
+3. Click on the “Create a New Cluster” button to create a new MongoDB cluster. Choose the cloud provider and region that best suits your needs. You can follow the provided instructions and configuration options to set up your cluster.
+4. Once your cluster is created and ready, go to the “Connect” section of your cluster.
+5. Under the “Choose a connection method” screen, select “Connect your application.”
+6. Select your preferred driver and version. MongoDB provides various drivers for different programming languages. Choose the one that matches the programming language you are using for your chatbot application.
+7. In the “Connection String Only” tab, you will find the connection string. This string includes all the necessary information to connect your application to the MongoDB cluster, such as the server address, port number, database name, and credentials.
+8. Click on the “Copy” button to copy the connection string to your clipboard.
+
+This connection will allow us to store and retrieve chat history for session-based chat functionality.
+Do not forget to replace your password in the connection string with your mongodb password.
+Add connection string into in .env file as `MONGODB_URL` variable.
+
+## Postgres: For Exisiting Customer Data
+You can follow mongodb for storing data but here I took postgres DB.
+
+Create table using below query:
+```bash
+CREATE TABLE customer (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    city VARCHAR(255),
+    email VARCHAR(255)
+);
+```
+Provide your postgres connection variables in .env file as `DB_NAME`, `DB_USER`, `DB_PASSWORD`, `DB_HOST`, `DB_PORT` variables.
+
+
+## Usage
+Clone this repository to your local machine:
+```bash
+git clone https://github.com/abhishekshingadiya/chatbot_conversational_form.git
+```
+
+Navigate to the project directory:
+```bash
+cd chatbot_conversational_form
+```
+
+Run the main script to start the application:
+```bash
+streamlit run api.py 
+```
+
+Provide customer id as session id and gpt_token(OPENAI_API_KEY). 
+
+
+![Screenshot 2023-11-06 at 12.29.05 PM.png](Screenshot%202023-11-06%20at%2012.29.05%E2%80%AFPM.png)
+This data will store in mongodb (As Conversations History) and postgres DB (Add/update customer data).
+
+![Screenshot 2023-11-06 at 12.34.26 PM.png](Screenshot%202023-11-06%20at%2012.34.26%E2%80%AFPM.png)
+
+## Enhancement
+- Introduce more details like age, contact_no in PersonalDetails class and map with customer table
+- Introduce sentiment analysis to understand customer mood and based on that can change chatbot response.
+- Support multiple language
+
+## Contact
+
+If you have any questions or suggestions, feel free to contact the project maintainer:
+
+- Abhishek Shingadiya
+- Email: abhishekshingadiya2543@email.com
---
+++ api.py
@@ -0,0 +1,267 @@
+import os
+import psycopg2
+import streamlit as st
+from langchain.chains import ConversationChain, create_tagging_chain_pydantic
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.memory import MongoDBChatMessageHistory
+from langchain.prompts.prompt import PromptTemplate
+from pydantic import BaseModel, Field
+from streamlit_chat import message
+from dotenv import load_dotenv
+
+load_dotenv()
+# setup up mongodb client
+mongodb_url = os.getenv("MONGODB_URL")
+db_name = os.getenv("DB_NAME")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+
+# setup openai
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+llm = None
+
+_DEFAULT_TEMPLATE = """You are an interactive conversational chatbot prepared by Learn-tube AI. Your goal is to collect user information in a conversational and non-intrusive manner, one piece at a time. When asking for details, explain why you need them, and be persuasive yet empathetic. Build rapport by transitioning into small talk when appropriate, but aim to gather data smoothly as the conversation progresses. If a user hesitates or is unsure, provide reassurance, offer alternatives, and if the user wishes to correct or update their details, be flexible and handle it trustworthily. If no information is needed, thank the user and ask how you can further assist them.
+
+Conversation Workflow:
+1. Explain the need for collecting information.
+2. Ask for one piece of information from the 'ask_for' list.
+3. If the user provides the information, confirm it empathetically and move to the next piece when appropriate.
+4. If the user hesitates, offer reassurance, and smoothly transition into small talk or other topics.
+5. When the 'ask_for' list is empty, thank the user and offer further assistance.
+6. Remember not to use greetings or list questions; keep it conversational.
+7. Don't revel information to the user unless they ask for it.
+8. Strictly don't use AI: and bot: kind of prefix in output.
+9. Don't ask the information which is already given.
+10. Make sure the information are in the format mentioned in the field description and let user know this
+
+Previous conversation:
+{history}
+Recent user input:
+{input}
+Information to ask for (do not ask as a list):
+### ask_for list: ask_for_list
+Available information of user: avl_info_list
+
+"""
+
+from pymongo import MongoClient
+from pymongo.collection import Collection
+# from pymongo.errors import ConnectionError
+from pymongo.errors import ConnectionFailure
+
+
+# MongoDB configuration
+mongo_uri = "mongodb+srv://chatbotuser:novell1320@aetiustestdb.xi8oe.mongodb.net/"
+db_name = "chatbotdb"
+collection_name = "customer"
+
+# Connect to MongoDB
+client = MongoClient(mongo_uri)
+db = client[db_name]
+collection = db[collection_name]
+
+def update_customer_table(session_id: object, data: object) -> object:
+    try:
+        result = collection.update_one(
+            {"_id": session_id},
+            {"$set": {"name": data['name'], "city": data['city'], "email": data['email'],"emiratesid": data['emiratesid'],"companyname":data['companyname']}},
+            upsert=True
+        )
+        return result.modified_count  # Returns the number of documents modified
+    except ConnectionError as e:
+        print(f"Error connecting to MongoDB: {e}")
+        return None
+
+def check_details_from_db(session_id: object) -> object:
+    try:
+        existing_row = collection.find_one({"_id": session_id})
+
+        if existing_row:
+            # If a document with the session_id exists, return the document
+            return existing_row
+        else:
+            # If the session_id doesn't exist, create a new document
+            result = collection.insert_one({"_id": session_id})
+            new_row = collection.find_one({"_id": session_id})
+            return new_row
+    except ConnectionError as e:
+        print(f"Error connecting to MongoDB: {e}")
+        return None
+ 
+
+
+# Define the PersonalDetails Pydantic model
+class PersonalDetails(BaseModel):
+    name: str = Field(
+        None,
+        description="The human name of the user.",
+    )
+    city: str = Field(
+        None,
+        description="The name of the city where someone lives",
+    )
+    email: str = Field(
+        None,
+        description="an email address that the person associates as theirs",
+    )
+    emiratesid: str = Field(
+        None,
+        description="an Emirates ID is the ID given in Dubai and unique to each person and should be in \
+                        123-456-789 format",
+    )
+    companyname: str = Field(
+        None,
+        description="its the company for which the user is working for",
+    )
+
+
+ner_chain = None
+
+
+# Define a function to check which fields are empty
+def check_what_is_empty(user_personal_details: object) -> object:
+    ask_for = []
+    for field, value in user_personal_details.dict().items():
+        if value in [None, "", 0]:
+            ask_for.append(field)
+    return ask_for
+
+
+# Define a function to update the non-empty details
+def add_non_empty_details(current_details: PersonalDetails, new_details: PersonalDetails) -> object:
+    non_empty_details = {k: v for k, v in new_details.dict().items() if v not in [None, ""]}
+    updated_details = current_details.copy(update=non_empty_details)
+    return updated_details
+
+
+def conversation_chat(input: object, session_id: object, llm=llm) -> object:
+    if session_id:
+        existing_info_from_db = check_details_from_db(session_id)
+        existing_info_of_user = PersonalDetails(**existing_info_from_db)
+    else:
+        existing_info_of_user = PersonalDetails()
+
+    message_history = MongoDBChatMessageHistory(
+        connection_string=mongodb_url, session_id=session_id
+    )
+    ner_chain = create_tagging_chain_pydantic(PersonalDetails, llm)
+    extractions = ner_chain.run(input)  # Extract information using your NER chain
+    existing_info_of_user = add_non_empty_details(existing_info_of_user, extractions)
+    existing_info_of_str = ", ".join(f"{k}={v}" for k, v in existing_info_of_user.dict().items() if v not in [None, ""])
+    ask_for = check_what_is_empty(existing_info_of_user)
+    update_customer_table(session_id, existing_info_of_user.dict())
+    memories = ConversationBufferMemory(k=3)
+
+    if len(message_history.messages):
+        memories.save_context(
+            {"input": message_history.messages[0].content},
+            {"output": message_history.messages[1].content}
+        )
+        PROMPT = PromptTemplate(
+            input_variables=["history", "input"],
+            template=_DEFAULT_TEMPLATE.replace("ask_for_list", f"{ask_for}").replace("avl_info_list",
+                                                                                     f"{existing_info_of_str}"),
+        )
+        conversation = ConversationChain(
+            llm=llm,
+            verbose=False,
+            prompt=PROMPT,
+            memory=memories
+        )
+        # first check if user has given all the details
+        conv = conversation.predict(input=input)
+
+        message_history.add_user_message(input)
+        message_history.add_ai_message(conv)
+
+        return conv
+
+    else:
+        PROMPT = PromptTemplate(
+            input_variables=["history", "input"],
+            template=_DEFAULT_TEMPLATE.replace("ask_for_list", f"{ask_for}").replace("avl_info_list",
+                                                                                     f"{existing_info_of_str}"),
+        )
+        conversation = ConversationChain(
+            llm=llm,
+            verbose=False,
+            prompt=PROMPT,
+            memory=memories,
+        )
+
+        conv = conversation.predict(input=input)
+        message_history.add_user_message(input)
+        message_history.add_ai_message(conv)
+
+        return conv
+
+
+st.title("Formfilling  ChatBot🧑🏽‍")
+
+
+# add field to sidebar gpt_token and session_id
+
+def initialize_session_state():
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
+
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hello! Ask me anything about 🤗"]
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hey! 👋"]
+
+    if 'session_id' not in st.session_state:
+        st.session_state['session_id'] = None
+
+
+def display_chat_history(session_id: object, llm):
+    if not session_id:
+        st.warning("Please enter a session ID in the sidebar")
+        return
+
+    reply_container = st.container()
+    container = st.container()
+    with container:
+        with st.form(key='my_form', clear_on_submit=True):
+            user_input = st.text_input("Question:", placeholder="Ask about you want to learn", key='input')
+            submit_button = st.form_submit_button(label='Send')
+
+        if submit_button and user_input:
+            output = conversation_chat(user_input, session_id=session_id, llm=llm)
+            st.session_state['past'].append(user_input)
+            st.session_state['generated'].append(output)
+
+    if st.session_state['generated']:
+        with reply_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="thumbs")
+                message(st.session_state["generated"][i], key=str(i), avatar_style="fun-emoji")
+
+
+def main():
+    initialize_session_state()
+    with st.sidebar:
+        with st.form(key='sidebar_form'):
+            session_id = st.text_input("Session ID", value=st.session_state.get('session_id', ''))
+            gpt_token = st.text_input("GPT Token", value='')
+            submit_button = st.form_submit_button(label='Update')
+
+        if submit_button:
+            st.session_state['session_id'] = session_id
+            if gpt_token:  # You'd typically not want to store the API key in session_state for security reasons
+                os.environ["OPENAI_API_KEY"] = gpt_token
+
+    if gpt_token:
+        os.environ["OPENAI_API_KEY"] = gpt_token
+        global llm
+        llm = ChatOpenAI(temperature=0)
+    display_chat_history(st.session_state['session_id'], llm=llm)
+
+
+if __name__ == "__main__":
+    main()
